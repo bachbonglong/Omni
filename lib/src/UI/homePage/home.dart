c35ac5fcd4c2966c7df4642f3f65c29b37bd9e6f
Bach Bong Long <bachbonglong@gmail.com>
2020-03-11 12:04:23 +0700

    Fix bug Login!!!

--- lib/src/UI/homePage/home.dart
+++ lib/src/UI/homePage/home.dart
@@ -4,7 +4,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key, this.user}) : super(key: key);
+
   final FirebaseUser user;
+
+  Center checkRole(DocumentSnapshot snapshot) {
+    if (snapshot.data == null) {
+      return Center(
+        child: Text('Xin chào ${user.email}' ),
+      );
+    }
+    if (snapshot.data['role'] == 'bachbonglong@gmail.com') {
+      return adminPage(snapshot);
+    } else {
+      return userPage(snapshot);
+    }
+  }
+
+  Center adminPage(DocumentSnapshot snapshot) {
+    return Center(
+        child: Text('${snapshot.data['role']} ${snapshot.data['name']}'));
+  }
+
+  Center userPage(DocumentSnapshot snapshot) {
+    return Center(child: Text(snapshot.data['name']));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,27 +52,5 @@ class Home extends StatelessWidget {
         
       ),
     );
-  }
-
-  Center checkRole(DocumentSnapshot snapshot) {
-    if (snapshot.data == null) {
-      return Center(
-        child: Text('Xin chào ${user.email}' ),
-      );
-    }
-    if (snapshot.data['role'] == 'admin') {
-      return adminPage(snapshot);
-    } else {
-      return userPage(snapshot);
-    }
-  }
-
-  Center adminPage(DocumentSnapshot snapshot) {
-    return Center(
-        child: Text('${snapshot.data['role']} ${snapshot.data['name']}'));
-  }
-
-  Center userPage(DocumentSnapshot snapshot) {
-    return Center(child: Text(snapshot.data['name']));
   }
 }
